e56f9d93aee1c5b07838ff99a34ba93432b3008d
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-14 10:43:45 -0600

    feature(backend): añade serializador, ViewSet y rutas para modelo Task

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 from .models import Project
+from .models import Task
 from .serializers import ProjectSerializer
+from .serializers import TaskSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
