8dc1a0c9ccb54e923d813bec416f445b47be9aa3
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-14 10:41:32 -0600

    chore(backend): configura CORS para desarrollo local
    De preferencia cambiar el enfoque para producción.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-zc0iq))lohal-(&_)prjrv)^x59qk#+c)!bkqw!a(w2k5(3#^u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Application definition
@@ -37,12 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'rest_framework',
     'projects',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
