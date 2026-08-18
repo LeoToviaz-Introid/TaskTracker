866edb3826f89b567a20788f53f0215bf95b40f4
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-18 14:53:13 -0600

    chore(backend): remueve admin site de Django

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,6 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
