0ef9eb3ba7ab175e9e7c79d20f36e6b9a78c5867
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-18 12:01:28 -0600

    feature(backend): configura modelo User personalizado como AUTH_USER_MODEL de Django

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'projects',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +63,19 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 TEMPLATES = [
     {
