eb902cdb0195fa6c503a21d49b1a5138f6556621
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-21 13:20:50 -0600

    fix(frontend): corrije falta de actualizaciones del Sidebar tras una operación CRUD

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,17 +1,26 @@
+import { serverRequest } from "@/app/server-api";
 import { Sidebar } from "@/components/Sidebar";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const projects = await getProjects();
   return (
     <>
-      <Sidebar />
+      <Sidebar projects={projects} />
       {/** contenedor grid para hacer dos filas con proporcion aproximada 30 70 */}
       <div className="grid grid-rows-[auto_1fr] gap-4 bg-gray-800 flex-1">
         <main className="flex-1">{children}</main>
       </div>
     </>
   );
+}
+
+async function getProjects() {
+  // obtener proyectos para el Sidebar
+  const res = await serverRequest("/projects/", "GET", undefined, "projects");
+  if (res.error) return [];
+  return res;
 }
