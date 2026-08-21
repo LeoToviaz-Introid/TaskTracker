ca8d504b26bbd9bd5c11ff9a12b78dce5e494815
Leonardo Toviaz <leonardo.toviaz@introid.com>
2026-08-21 10:00:11 -0600

    refactor(backend): cambia settings de Django para leer .env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,28 +10,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zc0iq))lohal-(&_)prjrv)^x59qk#+c)!bkqw!a(w2k5(3#^u'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") in 'True'
 
 ALLOWED_HOSTS = [
-    'localhost'
+    os.getenv("ALLOWED_HOSTS")
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    os.getenv("CORS_ALLOWED_ORIGINS"),
 ]
 
 
